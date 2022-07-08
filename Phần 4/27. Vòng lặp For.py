f4275a03c4df89a22cc3f420ebc056e7f8f4c6ab
Ngoc Bui Nguyen Bich <jayleenbui@gmail.com>
2022-07-08 22:39:16 +0700

    Practise For Loop

--- Phần 4/27. Vòng lặp For.py
+++ Phần 4/27. Vòng lặp For.py
@@ -1,7 +1,6 @@
 # Nhập vào một số. Nếu số đó là số lẻ thì cộng dồn các số lẻ, số chẵn thì cộng dồn các số chẵn.
 
 n = int(input('Nhập vào một số tự nhiên bất kỳ: '))
-i = 0
 s = 0
 if n % 2 == 0:
     for i in range(0, n+1, 2):
