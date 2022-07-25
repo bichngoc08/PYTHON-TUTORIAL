001cdc782a1e58c969960513a725b0c1f3efefb1
Ngoc Bui Nguyen Bich <jayleenbui@gmail.com>
2022-07-25 21:36:55 +0700

    Update BTRL4_Tính dãy số_Cách 2.py

--- Phần 4/BTRL4_Tính dãy số_Cách 2.py
+++ Phần 4/BTRL4_Tính dãy số_Cách 2.py
@@ -4,7 +4,7 @@ x = int(input("Nhập x: "))
 n = int(input("Nhập n: "))
 s = 0
 
-for i in range(1, n + 1, 1):2
+for i in range(1, n + 1, 1):
     mau = 1
     for j in range(1, i+1):
         mau = mau*j
