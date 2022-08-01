72e014b43fe4c120ecb8f70ce1e43b8483a50843
Ngoc Bui Nguyen Bich <jayleenbui@gmail.com>
2022-08-01 15:29:22 +0700

    Math basic Function

---
+++ Phần 5/37. Các hàm toán học.py
@@ -0,0 +1,13 @@
+
+from math import *
+
+x = 5
+y = 7
+print(pow(x, y))
+print(x**y)
+print(sqrt(25))
+do = 30
+print("30=> ", radians(do))
+rad = radians(do)
+print("rad(do)= ", degrees(rad))
+print(log10(100))
